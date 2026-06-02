e9e55d8f5e2dc7886f794ce25e15044850ff42ee
Ronaldo <ronaldoanthony12@gmail.com>
2026-06-02 09:37:02 -0400

    Interrupt 1

--- revision.ino
+++ revision.ino
@@ -1,32 +1,19 @@
-const int led = 9;
-int lastPot = 0;
-const int servo = 3;
+const int btn = 2;
+volatile bool flip = false;
 
-#include <Servo.h>
-
-Servo s;
-
-const int btn = 4;
-int btnState = 1;
-long int lastPress = 0;
 void setup() {
-  // put your setup code here, to run once:
-  s.attach(servo);
+  pinMode(btn, INPUT_PULLUP);
   Serial.begin(9600);
-  lastPot = analogRead(A0); //initial reading
- 
-
+  attachInterrupt(digitalPinToInterrupt(btn), motionISR, FALLING); //1 to 0
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //last pot
-  int raw = analogRead(A0);
-  if(abs(raw - lastPot) >=5) {
-    int angle = map(raw, 0, 1023, 0, 180);
-    s.write(angle);
-    Serial.print("Angle: "); Serial.println(angle);
-    //upadte last pot 
-    lastPot = raw;
+  if(flip == true) {
+    Serial.println("Door Opened.");
   }
+  flip = false;
+}
+
+void motionISR() {
+  flip = true;
 }
