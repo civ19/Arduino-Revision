06521b410f8c065bbe4c31e359217655d3bae3e6
Ronaldo <ronaldoanthony12@gmail.com>
2026-06-05 20:51:22 -0400

    LED Wireless Control

--- main.ino
+++ main.ino
@@ -1,10 +1,25 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(10, 11); // RX, TX
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("USB OK");
+  BT.begin(9600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Serial.println("Ready for LED Control.");
 }
 
 void loop() {
-  if (Serial.available()) {
-    Serial.write(Serial.read());
+
+  if (BT.available()) {
+    char data = BT.read(); //user input 
+    Serial.print("Received: ");
+    Serial.println(data);
+
+    if(data == '1') digitalWrite(13, HIGH);
+    else if (data == '0') digitalWrite(13, LOW);
+    
+    //basically, just begin the serials, do pinmode, take in user input, and thats iut
   }
 }
